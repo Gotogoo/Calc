99c0a3ae4a95446838709eb627e9ba134fa6fa7c
Gotogoo <fcliang@thoughtworks.com>
2017-07-27 12:48:36 +0800

    Initial Commit

---
+++ Calc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calc
+//
+//  Created by Facheng Liang  on 27/07/2017.
+//  Copyright © 2017 Facheng Liang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calc
+//
+//  Created by Facheng Liang  on 27/07/2017.
+//  Copyright © 2017 Facheng Liang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
